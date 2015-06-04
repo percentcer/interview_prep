df571b01faac1288d924fb488ed238bdcc2d4879
Spencer Miller <spence.miller@gmail.com>
2015-06-04 01:45:26 -0700

    Deleted run_tests.py

--- run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    from tests.test_arrays import TestArrayProblems
-    from tests.test_linked_lists import TestListProblems
-    from tests.test_stacks import TestStacksAndQueues
-    unittest.main(verbosity=2)
