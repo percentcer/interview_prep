be9468199dc2627971aa0131c85c55d5a6a6609c
Spencer Miller <spence.miller@gmail.com>
2015-06-08 20:27:29 -0700

    Added route checking, tree construction from sorted array, row linker, next node and iteration, common ancestor

--- logic/graphs.py
+++ logic/graphs.py
@@ -2,6 +2,9 @@ __author__ = 'percentcer'
 # Chapter 4
 # Graphs
 
+from collections import deque
+from logic.linked_lists import Node
+
 class NotSupportedError(Exception):
     pass
 
@@ -9,19 +12,37 @@ class TreeNode(object):
     def __init__(self, data):
         self._children = []
         self._data = data
+        self._parent = None
 
     def __eq__(self, other):
+        if other is None:
+            return False
+
         return self._data == other.data and self.children == other.children
 
     def add_child(self, other):
         self._children.append(other)
+        other._parent = self
 
     def is_leaf(self):
         return not any(self._children)
 
     @property
+    def parent(self):
+        return self._parent
+
+    def is_root(self):
+        return self._parent is None
+
+    @property
     def children(self):
         return self._children
+
+    @children.setter
+    def children(self, children):
+        self._children = children
+        for c in children:
+            c._parent = self
 
     @property
     def data(self):
@@ -39,6 +60,26 @@ class BinaryTreeNode(TreeNode):
         else:
             return self._children == other._children
 
+    def __str__(self):
+        return "{} ({}, {})".format(self.data, self.left, self.right)
+
+    def __repr__(self):
+        return str(self)
+
+    def __lt__(self, other):
+        return self.data < other.data
+
+    def __iter__(self):
+        if self.left:
+            for node in self.left:
+                yield node
+
+        yield self
+
+        if self.right:
+            for node in self.right:
+                yield node
+
     def add_child(self, other):
         raise NotSupportedError("Please use left or right for BinaryTreeNode")
 
@@ -53,6 +94,11 @@ class BinaryTreeNode(TreeNode):
     @right.setter
     def right(self, node):
         self._children[1] = node
+        if node is not None:
+            node._parent = self
+
+    def has_right(self):
+        return self.right is not None
 
     @property
     def left(self):
@@ -61,6 +107,11 @@ class BinaryTreeNode(TreeNode):
     @left.setter
     def left(self, node):
         self._children[0] = node
+        if node is not None:
+            node._parent = self
+
+    def has_left(self):
+        return self.left is not None
 
 
 def node_cons(obj):
@@ -101,7 +152,7 @@ def in_order(root):
     if root.left:
         ret.extend(in_order(root.left))
 
-    ret.append(root.data)
+    ret.append(root)
 
     if root.right:
         ret.extend(in_order(root.right))
@@ -112,7 +163,7 @@ def pre_order(root):
     if not isinstance(root, BinaryTreeNode):
         raise ValueError("Only binary nodes are supported!")
 
-    ret = [root.data]
+    ret = [root]
 
     if root.left:
         ret.extend(pre_order(root.left))
@@ -134,7 +185,7 @@ def post_order(root):
     if root.right:
         ret.extend(pre_order(root.right))
 
-    ret.append(root.data)
+    ret.append(root)
 
     return ret
 
@@ -154,3 +205,95 @@ def is_balanced(root):
         return True
     dleaves = depth_of_leaves(root)
     return max(dleaves) - min(dleaves) <= 1
+
+def has_route(start, end):
+    visited = set()
+    next_nodes = deque(start.children)
+
+    # nodes are unhashable, so let's use a simplified representation to track whether we've visited it
+    def rep(node):
+        return node.data, tuple(n.data for n in node.children)
+
+    while next_nodes:
+        current = next_nodes.popleft()
+
+        if rep(current) in visited:
+            continue
+
+        if current == end:
+            return True
+
+        visited.add(rep(current))
+        next_nodes.extend(current.children)
+
+    return False
+
+def make_tree_from_sorted(sa):
+    if len(sa) == 1:
+        return BinaryTreeNode(sa[0])
+
+    midpoint = len(sa)//2
+    r = BinaryTreeNode(sa[midpoint])
+    r.left = make_tree_from_sorted(sa[:midpoint])
+    if midpoint + 1 < len(sa):
+        r.right = make_tree_from_sorted(sa[midpoint+1:])
+
+    return r
+
+def linked_rows(bst):
+    rows = {}
+
+    def dfs(t, d):
+        head = rows.get(d)
+        if head:
+            head.append(Node(t.data))
+        else:
+            rows[d] = Node(t.data)
+        for c in t.children:
+            dfs(c, d + 1)
+
+    dfs(bst, 0)
+
+    return rows
+
+def next_value(node):
+    if node.right is not None:
+        return min(node.right)
+
+    cur = node
+
+    while cur.parent is not None:
+        cur = cur.parent
+        if cur.data > node.data:
+            return cur
+
+    # else, we hit root and return None
+
+def common_ancestor(left, right):
+
+    # discovered node IDs
+    ld = set()
+    rd = set()
+
+    while True:
+        if left is right:
+            return left
+        elif left.is_root() and right.is_root():
+            # disjoint trees, weird
+            return None
+
+        lid = id(left)
+        rid = id(right)
+
+        ld.add(lid)
+        rd.add(rid)
+
+        if lid in rd:
+            return left
+        if rid in ld:
+            return right
+
+        if left.parent:
+            left = left.parent
+        if right.parent:
+            right = right.parent
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -13,7 +13,22 @@ class TestGraphs(unittest.TestCase):
                 (111, ((222, ((333, ()),)),))
             ))
         )
+        self.binary_search_tree = binary_tree_constructor(
+            (42, (40, (37, (16, (), ()), ()), (41, (), ())), (58, (50, (49, (), ()), ()), (67, (), ())))
+        )
         self.empty_tree = tree_constructor(())
+        # 0_
+        # |  \
+        # 1   2 _
+        # | / | \|
+        # 3   4  5
+        nodes = [TreeNode(i) for i in range(6)]
+        nodes[0].children = [nodes[1], nodes[2]]
+        nodes[1].children = [nodes[3]]
+        nodes[2].children = [nodes[3], nodes[4], nodes[5]]
+        nodes[5].children = [nodes[2]]
+        self.looped_dag = nodes[0]
+        self.dag_nodes = nodes
 
     def tearDown(self):
         pass
@@ -32,15 +47,15 @@ class TestGraphs(unittest.TestCase):
 
     def test_in_order(self):
         desired = [0, 42, 1]
-        self.assertEqual(in_order(self.unordered_binary_tree), desired)
+        self.assertEqual([d.data for d in in_order(self.unordered_binary_tree)], desired)
 
     def test_pre_order(self):
         desired = [42, 0, 1]
-        self.assertEqual(pre_order(self.unordered_binary_tree), desired)
+        self.assertEqual([d.data for d in pre_order(self.unordered_binary_tree)], desired)
 
     def test_post_order(self):
         desired = [0, 1, 42]
-        self.assertEqual(post_order(self.unordered_binary_tree), desired)
+        self.assertEqual([d.data for d in post_order(self.unordered_binary_tree)], desired)
 
     def test_depth_of_leaves(self):
         self.assertEqual(depth_of_leaves(self.unordered_binary_tree), [1, 1])
@@ -57,3 +72,56 @@ class TestGraphs(unittest.TestCase):
     def test_binary_add_failure(self):
         b = BinaryTreeNode(0)
         self.assertRaises(NotSupportedError, b.add_child, 1)
+
+    def test_has_route(self):
+        self.assertTrue(has_route(self.dag_nodes[0], self.dag_nodes[3]))
+        self.assertTrue(has_route(self.dag_nodes[0], self.dag_nodes[5]))
+        self.assertTrue(has_route(self.dag_nodes[5], self.dag_nodes[2]))
+        self.assertTrue(has_route(self.dag_nodes[5], self.dag_nodes[3]))
+        self.assertTrue(has_route(self.dag_nodes[5], self.dag_nodes[5]))
+        self.assertFalse(has_route(self.dag_nodes[5], self.dag_nodes[0]))
+        self.assertFalse(has_route(self.dag_nodes[2], self.dag_nodes[1]))
+
+    def test_make_tree(self):
+        self.assertEqual(self.binary_search_tree, make_tree_from_sorted([16, 37, 40, 41, 42, 49, 50, 58, 67]))
+
+    def test_linked_rows(self):
+        actual = linked_rows(self.binary_search_tree)
+        desired = {
+            0: Node(42),
+            1: Node.from_list([40, 58]),
+            2: Node.from_list([37, 41, 50, 67]),
+            3: Node.from_list([16, 49])
+        }
+        self.assertEqual(actual, desired)
+
+    def test_next_value(self):
+        nodes = in_order(self.binary_search_tree)
+        _40 = nodes[2]
+        _41 = nodes[3]
+        _42 = nodes[4]
+        _49 = nodes[5]
+        _67 = nodes[8]
+        self.assertEqual(42, next_value(_41).data)
+        self.assertEqual(50, next_value(_49).data)
+        self.assertEqual(49, next_value(_42).data)
+        self.assertEqual(41, next_value(_40).data)
+        self.assertEqual(None, next_value(_67))
+
+    def test_common_ancestor(self):
+        nodes = in_order(self.binary_search_tree)
+        _16 = nodes[0]
+        _37 = nodes[1]
+        _40 = nodes[2]
+        _41 = nodes[3]
+        _42 = nodes[4]
+        _49 = nodes[5]
+        _50 = nodes[6]
+        _58 = nodes[7]
+        _67 = nodes[8]
+        self.assertEqual(common_ancestor(_16, _41), _40)
+        self.assertEqual(common_ancestor(_16, _49), _42)
+        self.assertEqual(common_ancestor(_16, _42), _42)
+        self.assertEqual(common_ancestor(_16, _37), _37)
+        self.assertEqual(common_ancestor(_37, _37), _37)
+        self.assertEqual(common_ancestor(_50, _67), _58)
